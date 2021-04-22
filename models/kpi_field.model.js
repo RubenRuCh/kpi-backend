51e4d217e83b30ffe77ac5354e55f2e1fd648c90
Rubén Rüger <ruben_27895@hotmail.com>
2021-04-22 18:39:31 +0200

    add primary key (id) to kpi_field

--- models/kpi_field.model.js
+++ models/kpi_field.model.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, Sequelize) => {
   const KPIField = sequelize.define('kpi_field', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
     value: {
       type: Sequelize.TEXT
     }
