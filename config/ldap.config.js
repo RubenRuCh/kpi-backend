6933b74cc06e3aa50f54b5de685b824bae6e67f4
rauloriol <eloriol@gmail.com>
2021-05-31 12:42:00 +0200

    Fix-> DockerFiles
    Few changes for optimice the deploy for API in rasperry,
    Change the ldap.config.js for enable the connection between container and App
    Ldap container -> https://github.com/osixia/docker-openldap
    docker run -p 389:389 -p 636:636 --name my-openldap-container --detach osixia/openldap:1.5.0
    cn=admin,dc=example,dc=org
    admin

--- config/ldap.config.js
+++ config/ldap.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/* module.exports = {
   URL: 'ldap://172.24.130.160:389',
   DC: 'dc=rruger,dc=dev',
   GROUP: 'Users',
   SEARCH_BY: 'uid',
+}; */
+
+/* Ip servidor */
+module.exports = {
+  URL: 'ldap://localhost:389',
+  DC: 'dc=example,dc=org',
+  GROUP: 'users',
+  SEARCH_BY: 'uid'
 };
--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-
-
-npx sequelize-cli db:migrate
-npx sequelize-cli db:seed:all
-
-
-npm run dev
