14b06bb382c57dd47b66fbd201ef5a466f015573
Rubén Rüger <ruben_27895@hotmail.com>
2021-06-02 20:16:17 +0200

    Idk man

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -40,7 +40,10 @@ exports.login = async (req, res) => {
     // let returnfromDB = user;
 
     // User formated with some important info + token
-    let returnfromDB = await userController.authenticateUser(req.body.user, req.body.password);
+    let returnfromDB = await userController.authenticateUser(
+      req.body.user,
+      req.body.password
+    );
 
     res.status(200).send(returnfromDB);
   } catch (err) {
